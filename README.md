d0028ffd56fbc6e3fbea6110d2951f7738068786
Luis <lumartinez2023@udec.cl>
2026-05-28 23:48:08 -0400

    docs: documentacion de parte del main y readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Tarea2_Estructura_de_datos
+_Luis Martinez Neira_<br>
+_2023427985_
+
+_Valentina Serón Canales_<br>
+_2020901589_
+
+## Sobre el entregable
+Este proyecto escrito en C++, permite crear un arbol general a partir de 10.000 archivos XML. Todo esto usando la librería tinyxml2 para extraer los metadatos de los libro
+
+## Sobre la compilación y ejecución
+Para probar este proyecto, se debe compilar con 
+```
+g++ main.cpp tree.cpp tinyxml2.cpp -o tarea2 -std=c++11
+```
+y para ejecutar `./tarea2`.
--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/* Archivo: main.cpp
+ Programa para revisar archivos XML de libros y construir una estructura de árbol.
+ Utiliza la librería tinyxml2 para extraer metadatos de libros desde una 
+ carpeta específica y almacenarlos en un árbol n-ario.
+*/
 #include <iostream>
 
 #include <dirent.h>  // Librería para leer directorios
@@ -9,13 +14,15 @@
 
 using namespace tinyxml2;
 
+// Lee un directorio de archivos XML y construye un árbol con los datos extraídos.
+// Retorna un puntero al árbol dinámico creado y poblado con los datos.
 Tree* crear_arbol_XML(){
     Tree* tree = new Tree();
     // Crear un nodo raiz
     tree->insert(0, "nodo_raiz", "0", 0, 0, "", "", 0.0, 0);
 
     
-    //Datos a utilizar de los XML
+    // Datos a utilizar de los XML
     int id;
     int id_padre;
     std::string titulo;
@@ -25,21 +32,22 @@ Tree* crear_arbol_XML(){
     std::string descripcion;
     double rating_promedio;
     int num_paginas;
-    //ruta de los XMLs
+    
+    // ruta de los XMLs
     std::string path = "books_xml";
     DIR *dir;
     struct dirent *read;
 
 
-    //Intentamos abrir la carpeta
+    // Intentamos abrir la carpeta
     if ((dir = opendir(path.c_str())) != NULL) {
         
-        //Leemos archivo por archivo
+        // Leemos archivo por archivo
         while ((read = readdir(dir)) != NULL) {
 
             std::string archivo = read->d_name;
             
-            //Ignoramos los directorios del sistema "." y ".."
+            // Ignoramos los directorios del sistema "." y ".."
             if (archivo != "." && archivo != "..") {
                 
                 
@@ -47,9 +55,11 @@ Tree* crear_arbol_XML(){
                 XMLDocument doc;
                 XMLError error = doc.LoadFile(ruta_completa.c_str());
 
+                // Si se carga con éxito, procedemos a extraer datos
                 if (error == XML_SUCCESS) {
                     std::cout << "Exito al cargar " << ruta_completa << "\n";
-                    //Tomamos todos los datos del libro
+                    
+                    // Tomamos todos los datos del libro
 
                     XMLElement* GoodreadsResponse = doc.FirstChildElement("GoodreadsResponse");
                     if(GoodreadsResponse == nullptr){
@@ -60,6 +70,8 @@ Tree* crear_arbol_XML(){
                         continue;
                     }
                     XMLElement* element;
+                    
+                    // Extraemos el título
                     element = book->FirstChildElement("title");
                     if(element != nullptr && element->GetText() != nullptr) {
                         titulo = element->GetText();
@@ -67,6 +79,8 @@ Tree* crear_arbol_XML(){
                     else{
                         titulo = "Sin titulo";
                     }
+                    
+                    // Extraemos el ISBN
                     element = book->FirstChildElement("isbn");
                     if(element != nullptr && element->GetText() != nullptr){
                         ISBN = element->GetText();
@@ -74,6 +88,8 @@ Tree* crear_arbol_XML(){
                     else{
                         ISBN = "0";
                     }
+                    
+                    // Extraemos el año de publicación
                     element = book->FirstChildElement("publication_year");
                     if(element != nullptr){
                         element->QueryIntText(&year_publicacion);
@@ -81,6 +97,8 @@ Tree* crear_arbol_XML(){
                     else{
                         year_publicacion = 0;
                     }
+                    
+                    // Extraemos el ID del libro (será el padre de los similares)
                     element = book->FirstChildElement("id");
                     if(element != nullptr){
                         element->QueryIntText(&id_padre);
@@ -88,6 +106,8 @@ Tree* crear_arbol_XML(){
                     else{
                         id_padre = -2;
                     }
+                    
+                    // Extraemos el código de idioma
                     element = book->FirstChildElement("language_code");
                     if(element != nullptr && element->GetText() != nullptr){
                         idioma = element->GetText();
@@ -95,6 +115,8 @@ Tree* crear_arbol_XML(){
                     else{
                         idioma = "N/A";
                     }
+                    
+                    // Extraemos la descripción
                     element = book->FirstChildElement("description");
                     if(element != nullptr && element->GetText() != nullptr){
                         descripcion = element->GetText();
@@ -102,6 +124,8 @@ Tree* crear_arbol_XML(){
                     else{
                         descripcion = "N/A";
                     }
+                    
+                    // Extraemos el rating promedio
                     element = book->FirstChildElement("average_rating");
                     if(element != nullptr){
                         element->QueryDoubleText(&rating_promedio);
@@ -109,6 +133,8 @@ Tree* crear_arbol_XML(){
                     else{
                         rating_promedio = 0.0;
                     }
+                    
+                    // Extraemos el número de páginas
                     element = book->FirstChildElement("num_pages");
                     if(element != nullptr){
                         element->QueryIntText(&num_paginas);
@@ -117,14 +143,17 @@ Tree* crear_arbol_XML(){
                         num_paginas = 0;
                     }
                     
+                    // Insertamos el libro principal en la raíz del árbol (id 0)
                     tree->insert(0,titulo, ISBN, year_publicacion, id_padre, idioma, descripcion, rating_promedio, num_paginas);
 
+                    // Buscamos si tiene libros similares
                     XMLElement* verificacion_similar_book = book->FirstChildElement("similar_books");
 
                     if(verificacion_similar_book != nullptr){
                         XMLElement* similar_book = verificacion_similar_book->FirstChildElement("book");
                         while(similar_book != nullptr){
-                            //Tomamos todos los datos de los libros similares
+                            
+                            // Tomamos todos los datos de los libros similares
                             element = similar_book->FirstChildElement("title");
                             if(element != nullptr && element->GetText() != nullptr) {
                                 titulo = element->GetText();
@@ -154,8 +183,10 @@ Tree* crear_arbol_XML(){
                                 continue;
                             }
                             
-
+                            // Insertamos el libro similar bajo el ID del libro padre
                             tree->insert(id_padre, titulo, ISBN, year_publicacion, id, "N/A", "N/A", 0.0, 0);
+                            
+                            // Pasamos al siguiente libro similar en la lista
                             similar_book = similar_book->NextSiblingElement("book");
                         }
                     }
@@ -174,12 +205,16 @@ Tree* crear_arbol_XML(){
     return tree;
 }
 
+// Recorre el árbol recursivamente para imprimir los IDs de los libros y contarlos.
 void listar(Tree* tree, int id, int* contador){
 
+    // Si no es la raíz, imprimimos y sumamos al contador
     if (id != 0) {
         std::cout << "ID Libro: " << id << "\n";
         *contador = *contador+1;
     }
+    
+    // Si es la raíz, iteramos por sus hijos
     if (id == 0){
         for(int id_hijo : tree->children(id)){
             listar(tree, id_hijo, contador);
@@ -187,7 +222,7 @@ void listar(Tree* tree, int id, int* contador){
     }
 }
 
-
+// Función principal. Inicia el proceso, lista y muestra el conteo total.
 int main() {
     Tree* tree = crear_arbol_XML();
     int contador = 0;
